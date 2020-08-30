be323a324861aa9a9b5363d6b9b6d4b3aa30be3c
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-08-30 13:04:33 +0700

    add les02

---
+++ chat-spring-21/lesson-2-spring-mvc/src/main/java/ru/geekbrains/persistance/User.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.persistance;
+
+public class User {
+
+    private int id;
+    private String login;
+    private String password;
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
